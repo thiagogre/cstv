a635dc80835b0c9cf0625efa10e5cf3d18874365
Thiago Gregorio <thiagoluiz_16@hotmail.com>
2024-01-04 10:16:20 -0300

    feat: Add theme colors for dark and light modes

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+const dark = {
+  PRIMARY_COLOR: '#000',
+  SECONDARY_COLOR: '#73737d',
+  TITLE_COLOR: '#fff',
+  BACKGROUND_COLOR: '#1D1E20',
+  BUTTON_COLOR: '#fff',
+};
+
+const light = {
+  PRIMARY_COLOR: '#fff',
+  SECONDARY_COLOR: '#73737d',
+  TITLE_COLOR: '#1D1E20',
+  BACKGROUND_COLOR: '#fcfcfc',
+  BUTTON_COLOR: '#1D1E20',
+};
+
+export {light, dark};
